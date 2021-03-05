e2863b2e39a06da08ad576016f21c0d572e129bc
giapdz <giapdz104@gmail.com>
2021-03-05 13:22:08 +0700

    magento

---
+++ app/etc/env.php
@@ -0,0 +1,83 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_m11g7c'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'a10b519780f5dea431ca85c4adac4fba'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'magentoUser',
+                'password' => 'admin123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'f73_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'f73_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => false
+    ],
+    'downloadable_domains' => [
+
+    ],
+    'cache_types' => [
+        'compiled_config' => 1,
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Tue, 02 Feb 2021 13:40:29 +0000'
+    ]
+];
